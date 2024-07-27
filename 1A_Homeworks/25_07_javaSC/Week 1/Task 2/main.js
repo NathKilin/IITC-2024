3b4654d5bb03cb040a067d5a2b820b399d20bda3
Nathan Kilinski <nathkilin@gmail.com>
2024-07-27 23:19:03 +0300

    homework

--- 1A_Homeworks/25_07_javaSC/Week 1/Task 2/main.js
+++ 1A_Homeworks/25_07_javaSC/Week 1/Task 2/main.js
@@ -1,1 +1,83 @@
-console.log('test')
+console.log('1')
+let x = 5
+console.log('answer = ', typeof x)
+console.log('')
+//////////////////////////////////////////
+console.log('2')
+console.log('answer = use a varaible "let", "const" or "var" :O to atribut a text between "".')
+//////////////////////////////////////////
+console.log('3')
+console.log('both are two different answers that represent absence of value but NULL is an answer to an variable that intentionally is zero or a null value. In other hand UNDEFINED is an answer for a variable without value')
+//////////////////////////////////////////
+console.log('4')
+console.log('just define a variable like LET X = 42; then type console.log(typeof x) and will be revealed the type of variable')
+//////////////////////////////////////////
+console.log('5')
+let result = 0/0
+console.log(result)
+//////////////////////////////////////////
+console.log('6')
+console.log('It´s a boolean type')
+//////////////////////////////////////////
+console.log('7')
+console.log("use a {} to declare an object")
+//////////////////////////////////////////
+console.log('8')
+console.log('NaN means Not_A_Number. It´s an answer to any mathematic oparation that can´t produce a significant number. Exemple: 0/0')
+//////////////////////////////////////////
+console.log('9')
+console.log('== represents an abstract iguality (like 1 == true and 0 == false) and === represents a strict true (12 === 12)')
+//////////////////////////////////////////
+console.log('10')
+console.log('just type Number("exemple_of_numer")')
+//////////////////////////////////////////
+console.log('11')
+console.log('a string')
+//////////////////////////////////////////
+console.log('12')
+console.log('use the operator "typeof" before the variable. (Exemple: typeof object')
+//////////////////////////////////////////
+console.log('13')
+console.log('just use ``')
+//////////////////////////////////////////
+console.log('14')
+console.log('thy\'re basically the same but with `` are able to offer multilines and the increment of variables using \'${}\'')
+//////////////////////////////////////////
+console.log('15')
+console.log('just add ""')
+//////////////////////////////////////////
+console.log('16')
+console.log(typeof null)
+//////////////////////////////////////////
+console.log('17')
+console.log('just add "const" before the variable')
+//////////////////////////////////////////
+console.log('18')
+console.log('There´s gonna be a concatenation and the number is converted to a string')
+//////////////////////////////////////////
+console.log('19')
+var result1 = 5>3
+console.log('data type is ', typeof result1)
+//////////////////////////////////////////
+console.log('20')
+console.log('just use backlash, single quote or doble quotes')
+//////////////////////////////////////////
+console.log('21')
+console.log(`
+VAR is function-scoped or global, variables declared with var are hoisted and reassignment is allowed
+LET is just block-scoped, hoisting will lead to temporal dead zone and can be reassigned
+CONST is block-scoped, hosting leads to temporal dead zone and reassignment is not allowed`)
+//////////////////////////////////////////
+console.log('22')
+console.log('just call the variable using "console.log" or use "typeof"')
+//////////////////////////////////////////
+console.log('23')
+result2 = 10 + "5"
+console.log('the result is ', result2)
+//////////////////////////////////////////
+console.log('24')
+console.log('just create variables that are discrebed as true or false')
+//////////////////////////////////////////
+console.log('25')
+console.log(`primitive data type is restored by value
+    objects are stored by reference`)
