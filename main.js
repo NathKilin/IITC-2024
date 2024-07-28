d5293dc508e4387b4bd439b58bc06b81dfb3c99f
Nathan Kilinski <nathkilin@gmail.com>
2024-07-28 13:01:35 +0300

    changes

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-console.log('testettsts')
+for (let i = 10; i > 0; i--){
+    console.log(i)}
+console.log('')
+
+for (let i = 2; i <= 20; i++){
+    if (i % 2 === 0){
+        console.log(i)
+    }
+}
+console.log('')
+let sum = 0
+for (i = 0; i<=10; x = i++){
+    sum += i;
+}
+console.log(sum
+)
+
+console.log('')
+
+for (let i = 0; i <= 10; i++){
+    console.log(5*i)}
+console.log('')
+
+for (let i = 0; i <= 5; i++){
+    console.log("*".repeat(i))}
+console.log('')
+
