1e54a80e8e5d42868436fde35b17ccb57325d1f6
Nathan Kilinski <nathkilin@gmail.com>
2024-07-28 20:54:34 +0300

    working in class

--- 28.07.24/main.js
+++ 28.07.24/main.js
@@ -25,3 +25,45 @@ for (let i = 0; i <= 5; i++){
     console.log("*".repeat(i))}
 console.log('')
 
+///////////////////////////////////////////////////////////
+
+//number = 1
+//while (number < 11){
+//    console.log(number++)
+//}
+//console.log('')
+//number = prompt('select a number: ')
+//while (number <101){
+//    console.log(number)
+//   number = number * 2
+//}
+///////////////////////////////////
+console.log('')
+let t = 1
+do {
+console.log(t++)
+}while(t<6)
+
+//////////////////////
+console.log('')
+for (let i = 1; i <10; i++){
+    if (i ===5) break;
+    console.log(i) }
+////////////////////////////
+console.log('')
+let foods = ['hamburguer','pizza','lasanha','soda','limon cake']
+console.log(foods[2])
+foods[1] = 'cake';
+console.log(foods);
+foods.push('spaggetti')
+console.log(foods)
+foods.shift();
+for (let i = 0; i< foods.length; i++){
+    console.log(foods[i])
+}
+console.log(foods.indexOf('limon cake'))
+
+for (let i = 0; i<10; i++){
+    sum = i+i
+}
+console.log(sum)
