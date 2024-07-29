0eafcb69f57e814affa0a3d448752dbed467eaad
Nathan Kilinski <nathkilin@gmail.com>
2024-07-29 15:44:19 +0300

    still on basics

--- 1A_Homeworks/29_07_JavaSC/Loops/Basics/main.js
+++ 1A_Homeworks/29_07_JavaSC/Loops/Basics/main.js
@@ -1,1 +1,130 @@
-console.log('blanlba')
+console.log('')
+console.log('Exercise 1')
+for (i = 1; i<6; i++){
+    console.log(i)
+}
+console.log('')
+console.log('Exercise 2')
+for (i = 0; i<10; i++){
+    console.log(i)
+}
+console.log('')
+console.log('Exercise 3')
+for (i = 10; i>0; i--){
+    console.log(i)
+}
+console.log('')
+console.log('Exercise 4')
+for (i = 1; i < 11; i++){
+    if (i%2 === 0){
+        console.log(i)
+    }
+}
+console.log('')
+console.log('Exercise 5')
+for (i = 1; i<11; i++){
+    if (i % 2 !== 0){
+        console.log(i)
+    }
+}
+
+console.log('')
+console.log('Exercise 6')
+for (i = 0; i < 16; i +=3){
+    console.log(i)
+}
+
+console.log('')
+console.log('Exercise 7')
+for (i = 1; i < 21; i +=2){
+    console.log(i)
+}
+
+console.log('')
+console.log('Exercise 8')
+for (i = 2; i < 21; i +=2){
+    console.log(i)}
+
+console.log('')
+console.log('Exercise 9')
+for (i = 10; i >= 0; i -=2){
+    console.log(i)}
+
+
+console.log('')
+console.log('Exercise 10')
+for (i = 5; i < 26; i +=5){
+    console.log(i)}
+
+console.log('')
+console.log('Exercise 11')
+let result = "";
+for (let i = 0; i < 5; i++) {
+    result += "*";
+}
+console.log(result);
+
+console.log('')
+console.log('Exercise 12')
+for (i = 1; i<4; i++){
+    console.log('Hello')}
+
+console.log('')
+console.log('Exercise 13')
+for (i = 1; i < 4; i++){
+    console.log(`${i}!`)
+}
+
+console.log('')
+console.log('Exercise 14')
+for (i = 65; i < 70; i++){
+    console.log(String.fromCharCode(i))
+}
+
+console.log('')
+console.log('Exercise 15')
+let two = "";
+for (let i = 0; i < 5; i++) {
+    two += " 2";
+}
+console.log(two)
+
+console.log('')
+console.log('Exercise 16')
+let array16 = [1, 2, 3, 4, 5]
+for (i = 0; i<5; i++){
+    console.log(array16[i])
+}
+
+console.log('')
+console.log('Exercise 17')
+let array17 = ['a', 'b', 'c', 'd']
+for (i = 0; i< 4; i++){
+    console.log(array17[i])
+}
+console.log('')
+console.log('Exercise 18')
+let array18 = [10, 20, 30, 40, 50]
+for (i = 4; i>= 0; i--){
+    console.log(array18[i])
+}
+
+console.log('')
+console.log('Exercise 19')
+let array19 = [1, 2, 3, 4, 5, 6]
+for (i = 0; i < 6; i+= 2){
+    console.log(array19[i])
+}
+
+console.log('')
+console.log('Exercise 20')
+let colors = ['red', 'green', 'blue']
+for (i = 0; i <3; i++){
+    console.log(`Color ${colors[i]}`)
+}
+
+console.log('')
+console.log('Exercise 21')
+for (i = 1; i < 6; i++){
+    console.log(i+=)
+}
