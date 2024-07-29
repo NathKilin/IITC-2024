81ae9f45d4dcc8a87125f9cb3fd6b4c237fc1e86
Nathan Kilinski <nathkilin@gmail.com>
2024-07-29 10:30:13 +0300

    ABC finished

--- 1A_Homeworks/29_07_JavaSC/ABC Challange/main.js
+++ 1A_Homeworks/29_07_JavaSC/ABC Challange/main.js
@@ -1,1 +1,11 @@
 // Your Code Goes Here:
+for (let i = 65; i <= 90; i++){
+    console.log(String.fromCharCode(i))}
+console.log(`
+    ;D`)
+/* 
+Explanation:
+I used the same code we learned yesterday using FOR
+Then I just set value "i" as 65 then I added +1 everytime that the number was lower or equal to 90
+I printed the number in a method that I found with GPT to switch the number for it string value
+And printed every result*/
