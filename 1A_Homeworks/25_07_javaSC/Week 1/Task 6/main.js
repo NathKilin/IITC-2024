887e01adb629a463e211c644a50fae54253b8291
Nathan Kilinski <nathkilin@gmail.com>
2024-07-29 09:49:38 +0300

    Abc

---
+++ 1A_Homeworks/25_07_javaSC/Week 1/Task 6/main.js
@@ -0,0 +1,50 @@
+/*
+Exercise 1
+8
+
+Exercise 2
+use operator +
+
+Exercise 3
+=
+
+Exercise 4
+6
+
+Exercise 5
+use operator*
+
+Exercise 6
+12
+
+Exercise 7
+/
+
+Exercise 8
+5
+
+Exercise 9
+the output is "infinity"
+
+Exercise 10
+using operator %
+
+Exercise 11
+2
+
+Exercise 12
+use ++ or +=1 or x + 1
+
+Exercise 13
+'x+= 1' is shorter and requires less space in the code
+
+Exercise 14
+use -- or -= or x - 1
+
+
+Exercise 15
+20
+
+Exercise 16
+
+*/
