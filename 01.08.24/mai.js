a87288f4ed7260c5ba9d2f54f7fed8e75f9cce73
Nathan Kilinski <nathkilin@gmail.com>
2024-08-01 09:44:27 +0300

    exemple

---
+++ 01.08.24/mai.js
@@ -0,0 +1,8 @@
+for (let i = 1; i <= 5; i ++){
+    console.log(`------------------------------
+
+        THIS IS INTERATION NUMBER ${i} OF THE MAIN (i) LOOP`)
+    for (j = 1; j <= 5; j++){
+        console.log(`This is iteration number ${j} of the inner (j) loop`);
+    }
+}
