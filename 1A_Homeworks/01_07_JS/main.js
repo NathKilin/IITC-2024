fb16110ca383396bafb6d371723fd6e35470296b
Nathan Kilinski <nathkilin@gmail.com>
2024-08-01 22:11:56 +0300

    starting homework

---
+++ 1A_Homeworks/01_07_JS/main.js
@@ -0,0 +1,30 @@
+//30 Basic 'for' Loop Exercises in JavaScript
+
+console.log('EXERCISE 1');
+list = ''
+function counting (n){
+    for (i = 1; i <= n; i++)
+        list += i + '... '
+        console.log(list)
+}
+counting(5)
+
+console.log('');
+console.log('EXERCISE 2');
+list = ''
+function counting(n){
+    for (i = 0; i <= n; i++)
+        list +=i + '... '
+        console.log(list)
+}
+counting(9)
+
+console.log('EXERCISE 3');
+list_10 = ''
+function counting_10(m){
+    for (i = m, i >= 1; i--;){
+        list_10 +=i + '... '
+    }
+    console.log(list_10)
+}
+counting_10(11)
