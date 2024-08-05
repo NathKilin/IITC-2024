deeaa0402bf47fa445480a9ceef03bf4bf8aede9
Nathan Kilinski <nathkilin@gmail.com>
2024-08-05 09:05:26 +0300

    STARTING THE DAY

---
+++ 04.08.24/Fitussi Exercises/main.js
@@ -0,0 +1,21 @@
+"use strict";
+console.log('EXERCISE 6');
+let smartphone = {
+    brand: 'apple',
+    model: 'Iphone',
+    storageGB: 128
+}
+console.log(smartphone);
+smartphone.model = 'IFini'
+console.log(smartphone);
+///////////////////////////
+ 
+console.log('');
+console.log('EXERCISE 7');
+let recipe = {
+    name: 'Ice',
+    igridients: ['Water', 'cold'],
+    preparationTime: 60
+}
+console.log(recipe.name, recipe.igridients[0]);
+
