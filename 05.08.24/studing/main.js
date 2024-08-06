938609ae52ebda9d41f410c106523e040e990a3c
Nathan Kilinski <nathkilin@gmail.com>
2024-08-06 12:02:18 +0300

    test finished

--- 05.08.24/studing/main.js
+++ 05.08.24/studing/main.js
@@ -12,7 +12,7 @@ function creatingTriangle(str){
 }
 creatingTriangle('*')
 */
-let myProduct = { 
+/*let myProduct = { 
     name: "laptop",
     price: 1000,
     isAvailable: true,
@@ -36,4 +36,29 @@ function getCategoriesLabels (product, categoryID){
         }
     }
 }
-getCategoriesLabels(myProduct, '2')
+
+let mylabel = getCategoryLabel(myProduct, "2");
+console.log("mylabel: ", mylabel);
+
+// TODO: Write a function that takes a product and return array of categories label and upperCase it
+function getCategoriesLabels(product) {
+  let categoriesLabels = [];
+  for (let i = 0; i < product.categories.length; i++) {
+    let label = product.categories[i].label.toUpperCase();
+    categoriesLabels.push(label);
+  }
+
+  return categoriesLabels;
+}
+
+let myCategoriesLabels = getCategoriesLabels(myProduct);
+console.log("myCategoriesLabels", myCategoriesLabels);*/
+let students = [
+    { id: "1", name: "omer", grade: 100 },
+    { id: "2", name: "avi", grade: 40 },
+    { id: "3", name: "gaga", grade: 80 },
+  ];
+
+  function getPassedStudents(arry, minGrade){
+    for (let i = 0)
+  }
