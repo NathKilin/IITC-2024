a6e5203d65b144ceaa8d77821f3281f46571e9a0
Nathan Kilinski <nathkilin@gmail.com>
2024-08-07 00:19:59 +0300

    going to sleep

--- 1A_Homeworks/06_08_JS/main.js
+++ 1A_Homeworks/06_08_JS/main.js
@@ -313,21 +313,43 @@ console.log("Uppercase Strings:", uppercaseStrings);
 
 /////////////////////////////////////////////////
 
+/*
 console.log('EXERCISE 13');
 
 // 🥵 Task 13: group strings by spaces occurences 🥵
 // TODO: Write a function to group strings by the number of spaces in the string.
 // The function should return an object where keys are the number of spaces and values are arrays of strings.
 
+const strings = [
+    "baba",
+    "my success",
+    "no more lives",
+    "and of session",
+    "good discussion",
+  ];
 
 function groupStringsBySpaces(strings) {
-  // your code here
+    let newObject = {};
+
+    for ( let i = 0; i < strings.length; i++){
+        let amountOfSpaces = 0
+
+        for (let j = 0; j < strings[i].length; j++){
+            if (strings[i][j] === ' '){
+                amountOfSpaces++    
+            }
+        }
+        if (!newObject[amountOfSpaces]){
+            newObject[amountOfSpaces] = []
+        }
+        newObject[amountOfSpaces].push(strings[i])
+    }
+    return newObject
 }
 
-
 const groupedStrings_1 = groupStringsBySpaces(strings);
-// console.log("Grouped Strings By Spaces:", groupedStrings_1);
-
+console.log("Grouped Strings By Spaces:", groupedStrings_1);
+*/
 
 /////////////////////////////////////////////////
 
@@ -344,7 +366,7 @@ function groupStringsByLength(strings) {
 
 
 const groupedStrings_2 = groupStringsByLength(strings);
-// console.log("Grouped Strings By Length:", groupedStrings);
+console.log("Grouped Strings By Length:", groupedStrings);
 
 
 /////////////////////////////////////////////////
